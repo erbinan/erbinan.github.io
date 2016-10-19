695d4a6a96919f42a79ca76b5db91a881b91286d
erbinan <WM@Macintosh.local>
2016-10-19 20:33:39 +0800

    33
    33

--- dangdang-book-angular/api/service.js
+++ dangdang-book-angular/api/service.js
@@ -3,14 +3,14 @@ app.factroy('commonService',['$http',function($http){
     servce.getData = function(type,callBack){
         $http({
             //type:
-            url:'../../data/book_' + type +'.json',
-            method:'get',
+            url:'../../data/book_' + type +'.json',///
+            method:'get',////
         })
         .then(function(res){
-            console.log('获取数据成功')
-            callBack(res);
+            console.log('获取数据成功')////
+            callBack(res);///
         },function(err){
-            console.dir(err);
+            console.dir(err);///
         })
     }
 }])
